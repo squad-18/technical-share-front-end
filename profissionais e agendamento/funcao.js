83164c396649aa2184af021c9286cc4093cdd247
Amanda Guerra <guerraamanda1@hotmail.com>
2022-04-13 11:11:36 -0300

    Teste JS para inserir as mentorias agendadas na página

--- profissionais e agendamento/funcao.js
+++ profissionais e agendamento/funcao.js
@@ -1,11 +1,50 @@
-$(document).ready(function () {
-  $('#btnSalvar').click(function () {
-    window.localStorage.setItem('Agendamento', JSON.stringify())
-  })
+// $(document).ready(function () {
+//   $('#btnSalvar').click(function () {
+//     window.localStorage.setItem('Agendamento', JSON.stringify())
+//   })
 
-  function getAgendamento() {
-    var agendamento = JSON.parse(window.localStorage.getItem('Agendamento'))
+//   function getAgendamento() {
+//     var agendamento = JSON.parse(window.localStorage.getItem('Agendamento'))
+//   }
+
+//   getAgendamento()
+// })
+
+var tabelaAgendamento = document.getElementById('tabelaAgendamento')
+
+var listaAgendamentos = []
+var mentoriaExemplo = {
+  mentor: 'Yago',
+  data: '14/04/2022',
+  hora: '11:00'
+}
+
+listaAgendamentos.push(mentoriaExemplo)
+
+function atualizarAgendamento() {
+  if (listaAgendamentos.length === 0) {
+    tabelaAgendamento.innerHTML =
+      '<tr><td class="text-muted">Nenhuma mentoria agendada</td></tr>'
+    return
   }
+  for (var i = 0; i < listaAgendamentos.length; i++) {
+    var agendamento = listaAgendamentos[i]
+    var linha = document.createElement('tr')
+    var celulaMentor = document.createElement('td')
+    var celulaData = document.createElement('td')
+    var celulaHora = document.createElement('td')
+    var celulaDesmarcar = document.createElement('td')
+    celulaMentor.innerText = agendamento.mentor
+    celulaData.innerText = agendamento.data
+    celulaHora.innerText = agendamento.hora
+    linha.appendChild(celulaMentor)
+    linha.appendChild(celulaData)
+    linha.appendChild(celulaHora)
+    linha.appendChild(celulaDesmarcar)
+    tabelaAgendamento.appendChild(linha)
+  }
+}
 
-  getAgendamento()
-})
+window.addEventListener('load', atualizarAgendamento())
+
+// não esta funcionando
